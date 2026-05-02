6df8750c6b57df66931872ff88dd327d8aaeb847
Flavio-dev <flavio2050dan@gmail.com>
2026-05-02 08:12:50 -0300

    Criação do modo escuro e claro como opção para o usuário
    Acrescentado a opção do modo escuro e modo claro, animação e fluidez as botões quando passado o cursor por cima e mudança de fluidez durante ao titulo ao mudar de modo.

--- Site/index.js
+++ Site/index.js
@@ -63,3 +63,71 @@ function saiu(){
     a.style.color = "lightgray";
     return;
 }
+
+let b = document.getElementById("mudarmodo");
+ b.addEventListener("click", butaob);
+ b.addEventListener("mouseenter", entrarb);
+ b.addEventListener("mouseleave", saiub);
+function butaob(){
+    b.innerText = "Trocado!"
+    return;
+}
+
+function entrarb(){
+    b.innerText = "Mudar Modo"
+    b.style.background = "lightgray";
+    b.style.color = "black";
+    return;    
+}
+
+function saiub(){
+    b.innerText = "Mudar Modo"
+    b.style.background = "rgb(80, 124, 124)";
+    b.style.color = "lightgray";
+    return;
+}
+
+
+function MudarModo(){
+    const modo = [
+        document.getElementById('titulo'),// 0
+        document.querySelector('body'),// 1
+        document.getElementById('formulario'),// 2
+        document.getElementById('nome'),// 3
+        document.getElementById('Nota1'),// 4
+        document.getElementById('Nota2'),// 5
+        document.getElementById('Nota3'), // 6
+        document.getElementById('B'),// 7
+        document.getElementById('resultado'), // 8
+        document.getElementById('mudarmodo')// 9
+    ];
+
+    if (modo[0].style.backgroundColor === 'white'){
+        modo[0].style.backgroundColor = 'darkslategray';
+        modo[0].style.color = 'black';
+        modo[1].style.backgroundColor = 'darkslategray';
+        modo[2].style.backgroundColor = 'darkslategray';
+        modo[2].style.color = 'black';
+        modo[3].style.backgroundColor = 'white';
+        modo[4].style.backgroundColor = 'white';
+        modo[4].style.color = 'black';
+        modo[5].style.backgroundColor = 'white';
+        modo[6].style.backgroundColor = 'dimgray';
+        modo[6].style.color = 'white';
+    }else{
+        modo[0].style.backgroundColor = 'white';
+        modo[0].style.color = 'darkslategray';
+        modo[1].style.backgroundColor = 'white';
+        modo[2].style.color = 'white';
+        modo[2].style.backgroundColor = 'white';
+        modo[3].style.backgroundColor = 'white';
+        modo[4].style.color = 'white';
+        modo[5].style.backgroundColor = 'white';
+        modo[6].style.color = 'white';
+        modo[7].style.backgroundColor = 'white';
+        modo[8].style.color = 'white';
+        modo[5].style.backgroundColor = 'white';
+        modo[6].style.backgroundColor = 'white';
+        modo[6].style.color = 'white';
+    }
+}
